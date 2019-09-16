f7f68a9fd66bc4a085ca5cc3f98791c6ed849563
Unknown <arnaud.piquet@wanadoo.fr>
2019-09-16 14:39:18 +0200

    Debut de la page d'accueil

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/logo.png">
+    <title>Perma Farmer</title>
+
+    <!--Template based on URL below-->
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Place your stylesheet here-->
+    <link href="/css/stylesheet.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <!--<img class="img-fluid" src="/logo.png"/>-->
+        <a class="navbar-brand" href="#">Perma Farmer</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbar">
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="" aria-label="Search">
+                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Rechercher</button>
+            </form>
+        </div>
+    </nav>
+
+    <main role="main" class="container">
+
+
+    </main><!-- /.container -->
+
+    <div class="container-fluid" style="margin-top: 50px;">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src="/logo.png"/>
+            </div>
+            <div class="col-md-8">
+                <h1>Perma Farmer</h1>
+                <p class="lead">Nous avons ouvert nos portes à Rennes en Mars 2018.<br/>De la volaille à la crémerie, des légumes aux boissons, en passant par l‘épicerie fine, ces produits sont tous de véritables produits fermiers, avec la qualité et les saveurs qui leur sont propres.<br> All you get is this text and a mostly barebones HTML document.</p>
+            </div>
+        </div>
+        
+        <div>
+            <h2>Nos produits</h2>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-2 offset-md-1"> <!-- image produit -->
+                <img/>
+                <h4>Oeufs</h4>
+                <p></p>
+            </div>
+            <div class="col-md-2"> <!-- image produit -->
+                <img/>
+                <h4>Pommes de terre</h4>
+                <p></p>
+            </div>
+            <div class="col-md-2"> <!-- image produit -->
+                <img/>
+                <h4>Carottes</h4>
+                <p></p>
+            </div>
+            <div class="col-md-2"> <!-- image produit -->
+                <img/>
+                <h4>Salades</h4>
+                <p></p>
+            </div>
+            <div class="col-md-2"> <!-- image produit -->
+                <img/>
+                <h4>Tomates</h4>
+                <p></p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-2 offset-md-1"> <!-- image produit -->
+                <img/>
+                <h4>Oignons</h4>
+                <p></p>
+            </div>
+            <div class="col-md-2"> <!-- image produit -->
+                <img/>
+                <h4>Courgettes</h4>
+                <p></p>
+            </div>
+            <div class="col-md-2"> <!-- image produit -->
+                <img/>
+                <h4>Poivrons</h4>
+                <p></p>
+            </div>
+            <div class="col-md-2"> <!-- image produit -->
+                <img/>
+                <h4>Aubergines</h4>
+                <p></p>
+            </div>
+            <div class="col-md-2"> <!-- image produit -->
+                <img/>
+                <h4>Brocolis</h4>
+                <p></p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
