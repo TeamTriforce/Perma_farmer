b69a7eb03645daf8c7d251ef573b5d430d192933
jeremy coley <jeremy.coley@tactfactory.com>
2019-09-19 16:58:03 +0200

    Continued workflow

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Class project with Jeremy, Matthias and Arnaud.
 
 MCD and databases dump can be found under the doc folder.
 Please ensure to set the correct database connection information in the DbConnector class.
+The information of the API are under the API folder in README.md file.
--- php/entities/AbstractEntity.php
+++ php/entities/AbstractEntity.php
@@ -10,8 +10,8 @@ abstract class AbstractEntity
 {
 
     /**
-     *	@param Array $params
-     *	@return void
+     * @param Array $params
+     * @return void
      */
     public function __construct(Array $params)
     {
@@ -19,43 +19,37 @@ abstract class AbstractEntity
     }
 
     /**
-     *	@param Array $data
-     *	@return void
+     * @param Array $data
+     * @return void
      */
     public function hydrate(Array $data)
     {
-        foreach ($data as $key => $value)
-        {
+        foreach ($data as $key => $value) {
             $field = explode("_", $key)[1];
             $method = "set" . ucfirst($field);
 
-            if (method_exists($this, $method))
-            {
+            if (method_exists($this, $method)) {
                 $this->$method($value);
             }
+
         }
     }
 
     /**
-     *	@param bool $assoc|false
-     *	@return array $objArray
+     * @param bool $assoc |false
+     * @return array $objArray
      */
     public function toArray(bool $assoc = false)
     {
         $objArray = array();
 
-        if ($assoc)
-        {
-            foreach (get_object_vars($this) as $attr => $value)
-            {
+        if ($assoc) {
+            foreach (get_object_vars($this) as $attr => $value) {
                 $field = strtolower(get_class($this)) . "_" . $attr;
                 $objArray[$field] = $value;
             }
-        }
-        else
-        {
-            foreach ($this as $attr => $value)
-            {
+        } else {
+            foreach ($this as $attr => $value) {
                 $objArray[] = $value;
             }
         }
