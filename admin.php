fe090223bc1dc21c1c55c74555e4a5d9da8a4465
Mathias <mathiaswandel35300@gmail.com>
2019-09-19 09:04:31 +0200

    page admin

---
+++ admin.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+        include("head.php");
+    ?>
+
+<body>
+    <?php
+        include("header.php");
+    ?>
+
+    <div class="container-fluid contact-form">
+        <div style="padding-top: 80px;">
+            <h2 class="text-center pt-3 pb-4">Administration</h2>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6" style="border: solid black 2px;">
+                    <h4 class="text-center">Liste utilisateurs</h4>
+                    <div class="row">
+                        <div class="col-md-10 offset-md-1" style="border: solid black 2px; margin-bottom:20px;">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <strong>Denis Legourrierec</strong>
+                                    <p>57 ans</p>
+                                    <p>Adresse</p>
+                                    <p>Ville</p>
+                                    <p>Mail</p>
+                                </div>
+                                <div class="col-md-4">
+                                    Ajouter<br>
+                                    Supprimer
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6" style="border: solid black 2px;">
+                    <h4 class="text-center">Dernières commandes</h4>
+                    <div class="row">
+                        <div class="col-md-10 offset-md-1" style="border: solid black 2px; margin-bottom:20px;">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <strong>Commande n°1059424</strong>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>En savoir plus</p>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <p>Eius populus ab incunabulis primis ad usque pueritiae tempus extremum, quod annis circumcluditur fere trecentis, circummurana pertulit bella, deinde aetatem ingressus.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6" style="border: solid black 2px;">
+                    <h4 class="text-center">Avis clients</h4>
+                    <div class="row">
+                        <div class="col-md-10 offset-md-1" style="border: solid black 2px; margin-bottom:20px;">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <strong>Denis Legourrierec</strong>
+                                    <p>Eius populus ab incunabulis primis ad usque pueritiae tempus extremum, quod annis circumcluditur fere trecentis, circummurana pertulit bella, deinde aetatem ingressus.</p>
+                                    <p>Note : 5/5</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6" style="border: solid black 2px;">
+                    <h4 class="text-center">Liste articles</h4>
+                    <div class="row">
+                        <div class="col-md-10 offset-md-1" style="border: solid black 2px; margin-bottom:20px;">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3>Oeufs</h3>
+                                    <strong>Description :</strong>
+                                    <p>Eius populus ab incunabulis primis ad usque pueritiae tempus extremum, quod annis circumcluditur fere trecentis, circummurana pertulit bella, deinde aetatem ingressus.</p>
+                                    <p><strong>Prix : </strong>1,99 € les 6 oeufs</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-10 offset-md-1" style="border: solid black 2px; margin-bottom:20px; width: 90%;">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h3>Tomates</h3>
+                                    <strong>Description :</strong>
+                                    <p>Eius populus ab incunabulis primis ad usque pueritiae tempus extremum, quod annis circumcluditur fere trecentis, circummurana pertulit bella, deinde aetatem ingressus.</p>
+                                    <p><strong>Prix : </strong>3,99 € le kg</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
