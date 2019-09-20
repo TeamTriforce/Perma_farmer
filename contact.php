88344bc3f76eaab8552cf6c90b6799e9ca53ddee
jeremy coley <jeremy.coley@tactfactory.com>
2019-09-20 13:42:14 +0200

    Added contact fields checks

--- contact.php
+++ contact.php
@@ -29,13 +29,13 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
-                        <input type="text" name="txtName" class="form-control" placeholder="Votre Nom *" value="" />
+                        <input type="text" name="txtName" class="form-control" placeholder="Votre Nom *" value="" required/>
                     </div>
                     <div class="form-group">
-                        <input type="text" name="txtEmail" class="form-control" placeholder="Votre Email *" value="" />
+                        <input type="email" name="txtEmail" class="form-control" placeholder="Votre Email *" value="" required/>
                     </div>
                     <div class="form-group">
-                        <input type="text" name="txtPhone" class="form-control" placeholder="Votre Téléphone *" value="" />
+                        <input type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" name="txtPhone" class="form-control" placeholder="Votre Téléphone *" value="" required/>
                     </div>
                     <div class="form-group">
                         <input type="submit" name="btnSubmit" class="btnContact" value="Envoyer" />
@@ -43,7 +43,7 @@
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
-                        <textarea name="txtMsg" class="form-control" placeholder="Message *" style="width: 100%; height: 150px;"></textarea>
+                        <textarea name="txtMsg" class="form-control" placeholder="Message *" required style="width: 100%; height: 150px;"></textarea>
                     </div>
                 </div>
             </div>
